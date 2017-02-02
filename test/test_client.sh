5df35fb97bcdb368b0cf31fdfa48b6026a2570f1
Bhargav Bhat <bhargavbhat@gmail.com>
2017-02-02 16:51:30 +0530

    AF Implementation

--- test/test_client.sh
+++ test/test_client.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-socat - udp:localhost:5080
+if [[ $1 == 6 ]]; then
+    socat - udp6:[::1]:5080
+else
+    socat - udp:localhost:5080
+fi
--- test/test_server.sh
+++ test/test_server.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-socat -v PIPE udp-recvfrom:6070,fork
+if [[ $1 == 6 ]]; then
+    socat -v PIPE udp6-recvfrom:6070,fork
+else
+    socat -v PIPE udp-recvfrom:6070,fork
+fi
